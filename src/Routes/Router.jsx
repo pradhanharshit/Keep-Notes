796b934a15f6fa194ba2a57cc793db71b919a899
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-12 15:40:25 +0530

    added page not found

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -12,13 +12,15 @@ import Home from "../frontend/components/Home/Home";
 import Archives from "../frontend/components/Archives/Archives";
 import Trash from "../frontend/components/Trash/Trash";
 import Labels from "../frontend/components/Labels/Labels";
+import PageNotFound from "../frontend/Pages/PageNotFound/PageNotFound";
 // import Layout from "../frontend/components/Layout/Layout";
 
 const Router = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="/landingpage" element={<LandingPage />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/landing" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
