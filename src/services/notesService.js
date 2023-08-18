4306e53fe6748d7f8c7f512b3d5ef4500d4ff9b1
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-18 18:20:11 +0530

    archive functions added

--- src/services/notesService.js
+++ src/services/notesService.js
@@ -1,10 +1,7 @@
-import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const addNoteHandler = async (note, authToken) => {
-  console.log(note);
-  // const { authToken } = useSelector((state) => state.auth);
-  // console.log("trying");
   try {
     const response = await axios.post(
       "/api/notes",
@@ -21,13 +18,62 @@ export const addNoteHandler = async (note, authToken) => {
   }
 };
 
-export const getAllNotes = async (note, authToken) => {
-  // const { authToken } = useSelector((state) => state.auth);
-  // console.log(note);
-  // console.log("getnotes authtoken", authToken);
-  // console.log("getallnotes called");
+export const getAllNotes = async (authToken) => {
   try {
     const response = await axios.get("/api/notes", {
+      headers: {
+        authorization: authToken,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const editNoteHandler = async (note, authToken) => {
+  try {
+    const response = await axios.post(
+      `api/notes/${note._id}`,
+      {
+        note,
+      },
+      {
+        headers: {
+          authorization: authToken,
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const archiveNoteHandler = async (note, authToken) => {
+  try {
+    // console.log;
+    const response = await axios.post(
+      `api/notes/archives/${note._id}`,
+      {
+        note,
+      },
+      {
+        headers: {
+          authorization: authToken,
+        },
+      }
+    );
+    // console.log("arch res", response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getAllArchivedNotes = async (authToken) => {
+  try {
+    const response = await axios.get("/api/archives", {
       headers: {
         authorization: authToken,
       },
--- src/store/notesSlice.js
+++ src/store/notesSlice.js
@@ -4,14 +4,37 @@ const notesSlice = createSlice({
   name: "notes",
   initialState: {
     mynotes: [],
+    archivedNotes: [],
     addNote: false,
+    newNoteRender: true,
+    archiveNoteRender: true,
   },
   reducers: {
     addNote(state) {
       state.addNote = !state.addNote;
     },
+    addNotesToArray(state, action) {
+      state.mynotes = [...action.payload];
+    },
+    renderNewNote(state) {
+      state.newNoteRender = !state.newNoteRender;
+      // console.log("render new", state.mynotes);
+    },
+    renderArchiveNote(state) {
+      state.archiveNoteRender = !state.archiveNoteRender;
+    },
+    addArchivesNotesToArray(state, action) {
+      state.archivedNotes = [...action.payload];
+      console.log(state.archivedNotes);
+    },
   },
 });
 
-export const { addNote } = notesSlice.actions;
+export const {
+  addNote,
+  renderNewNote,
+  addNotesToArray,
+  renderArchiveNote,
+  addArchivesNotesToArray,
+} = notesSlice.actions;
 export default notesSlice.reducer;
